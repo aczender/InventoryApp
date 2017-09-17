e664d5fa47dd61c5f6a028fd96ef824184d39875
aczender <czender.a@gmail.com>
2017-09-17 19:22:42 +0200

    V1.0

---
+++ app/src/main/java/com/example/andrew/inventoryapp/MainActivity.java
@@ -0,0 +1,9 @@
+package com.example.andrew.inventoryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
